67329eff8aa070ca576ea08bc967f025c845be19
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-07-19 17:50:18 +0700

    Update frozendash.py

--- frozendash.py
+++ frozendash.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly as px
+import plotly.express as px
 from sklearn.linear_model import LinearRegression
 
 # ----------------------
